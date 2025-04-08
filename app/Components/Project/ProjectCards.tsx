ec9a66cbdeaf9df99c8e0bbff4d1648921b6cd36
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-09 02:33:50 +0530

    Added custom fonts

--- app/Components/Project/ProjectCards.tsx
+++ app/Components/Project/ProjectCards.tsx
@@ -7,8 +7,8 @@ const ProjectCards: React.FC<{title: string, description: string}> = ({title, de
         <div className="bg-red-800 flex-1"></div>
         <div className="bg-green-800 flex-1 flex flex-col justify-between">
             <div className='pt-4 flex flex-col gap-4'>
-                <h3 className='text-2xl'>{title}</h3>
-                <p>{description}</p>
+                <h3 className='text-2xl font-heading'>{title}</h3>
+                <p className='font-text'>{description}</p>
             </div>
             <ul className='pb-4 flex gap-2 flex-wrap'>
                 <Tags title='UX Research'/>
