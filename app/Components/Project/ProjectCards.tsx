daef2d7cd6f44017cf46b8b5ebbe445b37aff883
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-09 01:47:44 +0530

    Added ProjectCards component

---
+++ app/Components/Project/ProjectCards.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectCards = () => {
+  return (
+    <div className="flex rounded-2xl overflow-hidden h-120 flex-col sm:h-60 sm:flex-row">
+        <div className="bg-red-800 flex-1">A</div>
+        <div className="bg-green-800 flex-1">B</div>
+    </div>
+  )
+}
+
+export default ProjectCards
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import ProjectCards from "./Components/Project/ProjectCards";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <div className="text-4xl text-blue-600">
         I’m Aman — blending UX research, design, and frontend development to build meaningful digital products.
+      </div>
+      <div className="flex flex-col w-full gap-8">
+        <ProjectCards />
+        <ProjectCards />
       </div>
     </div>
   );
