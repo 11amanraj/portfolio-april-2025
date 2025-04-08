59cce841abc3b14ccac01887ea6641ff2c16c4bd
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-09 02:35:47 +0530

    removed default fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./Components/Navigation/NavBar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Aman's Portfolio",
@@ -26,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-8`}
+        className="antialiased px-8"
       >
         <NavBar />
         {children}
