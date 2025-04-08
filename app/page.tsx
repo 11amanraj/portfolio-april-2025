3fe0792ee8b06c7655a7f121a72e5419d5d5c2a4
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-09 00:55:01 +0530

    Added hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-
+      <p className="text-4xl">I’m Aman — blending UX research, design, and frontend development to build meaningful digital products.</p>
     </div>
   );
 }
