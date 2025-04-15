e36b5a42534628acced2ddbc95053a8b84253e8e
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-16 00:06:44 +0530

    Updated hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ProjectCards from "./Components/Project/ProjectCards";
 import { Project } from '@/app/Types/Interface'
+import Image from "next/image";
 
 export default function Home() {
   const project_details: Project[] = [
@@ -54,7 +55,10 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 responsive-padding mb-8">
       <div className="min-h-[calc(100vh-88px)] flex flex-col gap-8 sm:max-w-200">
-        <div className="w-50 h-50 ml-16 sm:w-65 sm:h-65 sm:ml-20 bg-green-600 rounded-full"></div>
+        {/* <div className="w-50 h-50 ml-16 sm:w-65 sm:h-65 sm:ml-20 bg-green-600 rounded-full">
+          <img src="/hero-image.jpg" alt="hero-image" className="w-full h-full rounded-full overflow-clip"/>
+        </div> */}
+        <img src="/hero-image.jpg" alt="hero-image" className="w-50 h-50 ml-16 sm:w-70 sm:h-70 sm:ml-20 md:w-84 md:h-84 md:mb-16 rounded-full overflow-clip"/>
         <div className="text-3xl font-heading md:text-5xl">
           I’m Aman — blending <span className="text-primary">UX research</span>, <span className="text-primary">design</span>, and <span className="text-primary">frontend development</span> to build meaningful digital products.
         </div>
