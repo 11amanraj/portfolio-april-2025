c4ec4cd2a21488c12acad80df166632ff4314940
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-20 23:38:09 +0530

    minor changes

--- app/Components/Project/ProjectCards.tsx
+++ app/Components/Project/ProjectCards.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Tags from './Tags'
 import { Project } from '@/app/Types/Interface'
 
@@ -7,8 +8,10 @@ interface ProjectCardsProps {
 }
 
 const ProjectCards: React.FC<ProjectCardsProps> = ({ project }) => {
+  const [expandCard, SetExpandCard] = useState<boolean>(false)
+
   return (
-    <div className="flex rounded-2xl overflow-hidden h-140 flex-col sm:h-70 sm:flex-row">
+    <div className="flex rounded-2xl overflow-hidden h-140 flex-col sm:h-140 sm:flex-row">
         <div className="bg-red-800 rounded-2xl flex-1 overflow-hidden">
           <img src={project.image} alt={project.title} className='h-full object-cover'/>
         </div>
@@ -25,16 +28,6 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({ project }) => {
                 <Tags title='Frontend'/>
             </ul>
             <p className='font-text'>{project.description}</p>
-            {/* <div className='pt-4 flex flex-col gap-4'>
-            </div> */}
-          {/* <div className='flex gap-4'>
-            <span>Open Case Study</span>
-            <span>
-              <a href={project.caseStudyLink} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/open_in_new.svg" alt="open-in-new" />
-              </a>
-            </span>
-          </div> */}
         </div>
     </div>
   )
