908f656c292f7629208df65baef21a372d96dbda
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-21 20:11:50 +0530

    Added ProjectsList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,7 @@
-import ProjectCards from "./Components/Project/ProjectCards";
 import { Project } from '@/app/Types/Interface'
-import Image from "next/image";
+import ProjectsList from "./Components/Project/ProjectsList";
 
 export default function Home() {
-  const project_details: Project[] = [
-    {
-      id: 0,
-      title: 'Academic Info System for IIT Roorkee',
-      description: 'Identified communication gaps in IIT Roorkee’s academic ecosystem and designed a centralized platform using user interviews and the Double Diamond approach.',
-      tags: ['UX Resarch, UI, Frontend'],
-      caseStudyLink: 'https://www.behance.net/gallery/221113241/Academic-Information-System', 
-      resourceLink: 'www.google.com',
-      image: '/project-thumbnails/ais-portal.jpg'
-    },
-    {
-      id: 1,
-      title: 'GPay Redesign: Smarter Splits, Clearer Expenses',
-      description: 'An experience-focused redesign that simplifies how users split costs and monitor spending within the Google Pay ecosystem.',
-      tags: ['UX Resarch, UI, Frontend'],
-      caseStudyLink: 'https://www.behance.net/gallery/211761657/GPay-Redesign', 
-      resourceLink: 'www.google.com',
-      image: '/project-thumbnails/gpay-redesign.jpg'
-    },
-    {
-      id: 2,
-      title: 'Researching Mobile Accessibility for the Elderly',
-      description: 'Through user interviews and observational research, we uncovered barriers faced by older adults in mobile usage — culminating in a comprehensive set of accessibility guidelines.',
-      tags: ['UX Resarch, UI, Frontend'],
-      caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-      resourceLink: 'www.google.com',
-      image: '/project-thumbnails/gpay-redesign.jpg'
-    },
-    {
-      id: 3,
-      title: 'A Designer–Developer’s Portfolio, Built with Code',
-      description: 'Designed and developed my own portfolio using Next.js, TailwindCSS, and TypeScript — blending aesthetics with performance and thoughtful UX.',
-      tags: ['UX Resarch, UI, Frontend'],
-      caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-      resourceLink: 'www.google.com',
-      image: '/project-thumbnails/gpay-redesign.jpg'
-    },
-    {
-      id: 4,
-      title: 'Starbound: A Static Site for a Space Tourism Brand',
-      description: 'Designed and developed a concept website with custom layouts and page routing, highlighting creative theming and component reuse.',
-      tags: ['UX Resarch, UI, Frontend'],
-      caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-      resourceLink: 'www.google.com',
-      image: '/project-thumbnails/gpay-redesign.jpg'
-    }
-
-  ]
-
   return (
     <div className="flex flex-col gap-8">
       <div className="min-h-[calc(100vh-88px)] responsive-padding flex flex-col gap-8 sm:max-w-200">
@@ -64,9 +14,7 @@ export default function Home() {
         </div>
         <div className="text-xl font-heading md:text-3xl">I aim to craft experiences that are not only functional, but emotionally resonant and inclusive.</div>
       </div>
-      <div className="flex flex-col w-full gap-8 responsive-padding pb-8 bg-primary">
-        {project_details.map(project => <ProjectCards key={project.id} project={project}/>)}
-      </div>
+      <ProjectsList />
     </div>
   );
 }
