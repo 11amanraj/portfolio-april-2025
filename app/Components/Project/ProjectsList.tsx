d47a6c3d4811f1df018dcff05bd24167add83af0
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-21 20:21:30 +0530

    Added styling to ProjectsList component

--- app/Components/Project/ProjectsList.tsx
+++ app/Components/Project/ProjectsList.tsx
@@ -53,8 +53,12 @@ const ProjectsList = () => {
       ]
 
   return (
-    <div className="flex flex-col w-full gap-8 responsive-padding pb-8 bg-primary">
-        {project_details.map(project => <ProjectCards key={project.id} project={project}/>)}
+    <div className="flex flex-col w-full gap-8 py-16 responsive-padding bg-primary">
+        <h3 className='text-[96px] text-background'>Projects</h3>
+        {project_details
+            .map(project => 
+                <ProjectCards key={project.id} project={project}/>
+            )}
       </div>
   )
 }
