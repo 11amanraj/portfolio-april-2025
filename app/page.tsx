1ea57b868758db5f6522381cd71ac600071f49a8
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-01 20:07:18 +0530

    Extracted HeroSection code into its own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { Project } from '@/app/Types/Interface'
 import ProjectsList from "./Components/Project/ProjectsList";
+import HeroSection from './Components/Hero/HeroSection';
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
-      <div className="min-h-[calc(100vh-88px)] responsive-padding flex flex-col justify-center gap-8 sm:max-w-250 lg:max-w-280 xl:max-w-320">
+      <HeroSection />
+      {/* <div className="min-h-[calc(100vh-88px)] responsive-padding flex flex-col justify-center gap-8 sm:max-w-250 lg:max-w-280 xl:max-w-320">
         <div className="text-3xl font-heading md:text-[64px]">
           Hey! I’m Aman — blending <span className="text-primary">UX research</span>, <span className="text-primary">design</span>, and <span className="text-primary">frontend development</span> to build meaningful digital products.
         </div>
         <div className="text-xl font-heading md:text-4xl">I aim to craft experiences that are not only functional, but emotionally resonant and inclusive.</div>
-      </div>
+      </div> */}
       <ProjectsList />
     </div>
   );
