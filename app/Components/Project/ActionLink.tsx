363b76542fb48f35d5b0200b8d7986d1ff01f3d4
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-26 22:02:02 +0530

    Changed rhe color for action link

--- app/Components/Project/ActionLink.tsx
+++ app/Components/Project/ActionLink.tsx
@@ -34,8 +34,8 @@ const ActionLink: React.FC<{title: string, link: string, isPrimary: boolean}> = 
                 <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 -960 960 960" 
-                className='w-full'
-                fill='#F56565' 
+                className='w-full text-background'
+                fill='#0A84FF'
             >
                 <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"/>
             </svg>
