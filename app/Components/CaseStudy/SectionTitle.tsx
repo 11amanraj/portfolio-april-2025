53f3e835407031e12e6114dae3679e483a4b6140
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-07 00:25:07 +0530

    Removed extra code

--- app/Components/CaseStudy/SectionTitle.tsx
+++ app/Components/CaseStudy/SectionTitle.tsx
@@ -28,26 +28,6 @@ const SectionTitle: React.FC<{ sectionNumber: string, titleDetail: titleSection[
                     </span>
                 </div>
             )}
-
-            {/* <div className='flex flex-col basis-0 flex-[2] gap-2'>
-                <span className='text-right'>
-                    Mini Title
-                </span>
-                <span className='h-0.5 bg-red-400'></span>
-                <h2 className='text-3xl max-w-120 text-right'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </h2>
-            </div>
-
-            <div className='flex flex-col basis-0 flex-[2] gap-2'>
-                <span className='text-right'>
-                    Mini Title
-                </span>
-                <span className='h-0.5 bg-red-400'></span>
-                <h2 className='text-3xl max-w-120 text-right'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </h2>
-            </div> */}
         </div>
     )
 }
