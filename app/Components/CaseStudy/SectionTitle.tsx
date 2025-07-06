af89a3c278ccfc6290f91363add443b1582054f6
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-07 00:31:37 +0530

    Added flex wrap

--- app/Components/CaseStudy/SectionTitle.tsx
+++ app/Components/CaseStudy/SectionTitle.tsx
@@ -3,7 +3,7 @@ import { titleSection } from '@/app/Types/Interface'
 
 const SectionTitle: React.FC<{ sectionNumber: string, titleDetail: titleSection[] }> = ({ sectionNumber, titleDetail }) => {
     return (
-        <div className='flex font-mono justify-between'>
+        <div className='flex flex-wrap gap-y-8 font-mono justify-between'>
             <div className='flex flex-col basis-0 flex-[1] gap-2'>
                 <span>
                     {sectionNumber}
@@ -12,22 +12,24 @@ const SectionTitle: React.FC<{ sectionNumber: string, titleDetail: titleSection[
                 <h2 className='text-5xl'>{titleDetail[0].text}</h2>
             </div>
 
-            {titleDetail.slice(1).map(detail =>
-                <div key={detail.id} className='flex flex-col basis-0 flex-[2] gap-2'>
-                    {detail.header ?
-                        <span className='text-right'>
-                            {detail.header}
-                        </span> :
-                        <span className='text-right'>
-                            &nbsp;
+            <div className='flex justify-between'>
+                {titleDetail.slice(1).map(detail =>
+                    <div key={detail.id} className='flex flex-col gap-2'>
+                        {detail.header ?
+                            <span className='text-right'>
+                                {detail.header}
+                            </span> :
+                            <span className='text-right'>
+                                &nbsp;
+                            </span>
+                        }
+                        <span className='h-0.5 bg-red-400'></span>
+                        <span className='text-3xl w-100 self-end text-right'>
+                            {detail.text}
                         </span>
-                    }
-                    <span className='h-0.5 bg-red-400'></span>
-                    <span className='w-full text-3xl max-w-100 self-end text-right'>
-                        {detail.text}
-                    </span>
-                </div>
-            )}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
