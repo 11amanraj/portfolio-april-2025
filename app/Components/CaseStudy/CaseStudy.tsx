3b555e93066696426a7172f67e745df3cf39b71a
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-06 21:25:50 +0530

    Added styling to navigation links

--- app/Components/CaseStudy/CaseStudy.tsx
+++ app/Components/CaseStudy/CaseStudy.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react'
 import PageNavigation from './PageNavigation'
 
 const CaseStudy = () => {
+    const sections = [
+        {
+            key: 0,
+            id: 'section-a',
+            title: 'Section A'
+        },
+        {
+            key: 1,
+            id: 'section-b',
+            title: 'Section B'
+        },
+        {
+            key: 2,
+            id: 'section-c',
+            title: 'Section C'
+        },
+        {
+            key: 3,
+            id: 'section-d',
+            title: 'Section D'
+        },
+    ]
+
+    const [activeSection, SetActiveSection] = useState(sections[0].id)
+
   return (
     <div>
-        <PageNavigation />
+        <PageNavigation activeSection={activeSection} />
         <section id='section-a' className='h-screen w-full bg-green-800'>Section A</section>
         <section id='section-b' className='h-screen w-full bg-red-800'>Section B</section>
         <section id='section-c' className='h-screen w-full bg-black'>Section C</section>
--- app/Components/CaseStudy/PageNavigation.tsx
+++ app/Components/CaseStudy/PageNavigation.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const PageNavigation = () => {
-  return (
+const PageNavigation: React.FC<{activeSection: string}> = ({activeSection}) => {
+  console.log(activeSection)
+  
+    return (
     <div className='fixed left-0 right-0 top-0 flex gap-8 p-4 bg-yellow-500 text-black'>
-        <a href='#section-a'>Section A</a>
-        <a href='#section-b'>Section B</a>
-        <a href='#section-c'>Section C</a>
-        <a href='#section-d'>Section D</a>
+        <a href='#section-a' className={`${activeSection === 'section-a' ? 'text-blue-700' : 'text-gray-700'}`}>Section A</a>
+        <a href='#section-b' className={`${activeSection === 'section-b' ? 'text-blue-700' : 'text-gray-700'}`}>Section B</a>
+        <a href='#section-c' className={`${activeSection === 'section-c' ? 'text-blue-700' : 'text-gray-700'}`}>Section C</a>
+        <a href='#section-d' className={`${activeSection === 'section-d' ? 'text-blue-700' : 'text-gray-700'}`}>Section D</a>
     </div>
   )
 }
