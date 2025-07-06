e8995626179e6e44160a0e9f9ea526137c2171f5
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-06 21:09:39 +0530

    gave styling to the navigation bar

--- app/Components/CaseStudy/PageNavigation.tsx
+++ app/Components/CaseStudy/PageNavigation.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageNavigation = () => {
   return (
-    <div>
+    <div className='fixed left-0 right-0 top-0 flex gap-8 p-4 bg-yellow-500 text-black'>
         <a href='#section-a'>Section A</a>
         <a href='#section-b'>Section B</a>
         <a href='#section-c'>Section C</a>
