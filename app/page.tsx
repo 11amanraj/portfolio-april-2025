ba0c793cb42823e045589198207391c0f10992fb
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-26 15:33:31 +0530

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import ProjectsList from "./Components/Project/ProjectsList";
 import HeroSection from './Components/Hero/HeroSection';
-import CaseStudy from "./Components/CaseStudy/CaseStudy";
+// import CaseStudy from "./Components/CaseStudy/CaseStudy";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 overflow-y-scroll snap-y snap-mandatory">
-      <CaseStudy />
-      {/* <HeroSection />
-      <ProjectsList /> */}
+      {/* <CaseStudy /> */}
+      <HeroSection />
+      <ProjectsList />
     </div>
   );
 }
