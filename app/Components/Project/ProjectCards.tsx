e62f54383a5743ac2175b1d3753de479ee047b2b
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-07-29 11:58:54 +0530

    Changes for final

--- app/Components/Project/ProjectCards.tsx
+++ app/Components/Project/ProjectCards.tsx
@@ -6,28 +6,37 @@ import ActionLink from './ActionLink';
 
 interface ProjectCardsProps {
   project: Project,
-  isReverse: boolean
+  isReverse: boolean,
+  designCaseStudy: boolean
 }
 
-const ProjectCards: React.FC<ProjectCardsProps> = ({ project, isReverse }) => {
+const ProjectCards: React.FC<ProjectCardsProps> = ({ project, isReverse, designCaseStudy }) => {
 
   return (
     <li className={`max-w-300 p-8 rounded-4xl bg-white/70 flex flex-col ${isReverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} items-center gap-8 md:gap-16`}>
-        <div className="rounded-2xl w-full flex-1 overflow-hidden">
-          <img src={project.image} alt={project.title} className='h-full object-cover'/>
+      <div className="rounded-2xl w-full flex-1 overflow-hidden">
+        <img src={project.image} alt={project.title} className='h-full object-cover' />
+      </div>
+      <div className='flex flex-col flex-1 gap-3 sm:gap-4'>
+        <h4 className='font-text font-bold text-2xl sm:text-3xl xl:text-4xl'>{project.title}</h4>
+        <div className='flex gap-3 flex-wrap'>
+          {designCaseStudy
+            ? <ActionLink title='Read Case Study' isPrimary={true} link={project.caseStudyLink} />
+            : <>
+              <ActionLink title='View Website' isPrimary={true} link={project.caseStudyLink} />
+              <ActionLink title='Github' isPrimary={false} link={project.caseStudyLink} />
+            </>
+            // : <ActionLink title='Github' isPrimary={false} link={project.caseStudyLink} />
+          }
+          {/* <ActionLink title='Read Case Study' isPrimary={true} link={project.caseStudyLink} />
+          <ActionLink title='Github' isPrimary={false} link={project.caseStudyLink} /> */}
         </div>
-        <div className='flex flex-col flex-1 gap-3 sm:gap-4'>
-          <h4 className='font-text font-bold text-2xl sm:text-3xl xl:text-4xl'>{project.title}</h4>
-          <div className='flex gap-3 flex-wrap'>
-            <ActionLink title='Read Case Study' isPrimary={true} link={project.caseStudyLink}/>
-            <ActionLink title='Github' isPrimary={false} link={project.caseStudyLink}/>
-          </div>
-          <ul className='flex gap-3'>
-            <Tags title='UX Research'/>
-            <Tags title='UI'/>
-            <Tags title='Frontend'/>
-          </ul>
-        </div>
+        <ul className='flex gap-3'>
+          <Tags title='UX Research' />
+          <Tags title='UI' />
+          <Tags title='Frontend' />
+        </ul>
+      </div>
     </li>
   )
 }
--- app/Components/Project/ProjectsList.tsx
+++ app/Components/Project/ProjectsList.tsx
@@ -3,65 +3,62 @@ import ProjectCards from './ProjectCards'
 import { Project } from '@/app/Types/Interface'
 
 const ProjectsList = () => {
-    const project_details: Project[] = [
-        {
-          id: 0,
-          title: 'Academic Info System for IIT Roorkee',
-          description: 'Identified communication gaps in IIT Roorkee’s academic ecosystem and designed a centralized platform using user interviews and the Double Diamond approach.',
-          tags: ['UX Resarch, UI, Frontend'],
-          caseStudyLink: 'https://www.behance.net/gallery/221113241/Academic-Information-System', 
-          resourceLink: 'www.google.com',
-          image: '/project-thumbnails/ais-portal.jpg'
-        },
-        {
-          id: 1,
-          title: 'GPay Redesign: Smarter Splits, Clearer Expenses',
-          description: 'An experience-focused redesign that simplifies how users split costs and monitor spending within the Google Pay ecosystem.',
-          tags: ['UX Resarch, UI, Frontend'],
-          caseStudyLink: 'https://www.behance.net/gallery/211761657/GPay-Redesign', 
-          resourceLink: 'www.google.com',
-          image: '/project-thumbnails/gpay-redesign.jpg'
-        },
-        {
-          id: 2,
-          title: 'Researching Mobile Accessibility for the Elderly',
-          description: 'Through user interviews and observational research, we uncovered barriers faced by older adults in mobile usage — culminating in a comprehensive set of accessibility guidelines.',
-          tags: ['UX Resarch, UI, Frontend'],
-          caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-          resourceLink: 'www.google.com',
-          image: '/project-thumbnails/gpay-redesign.jpg'
-        },
-        {
-          id: 3,
-          title: 'A Designer–Developer’s Portfolio, Built with Code',
-          description: 'Designed and developed my own portfolio using Next.js, TailwindCSS, and TypeScript — blending aesthetics with performance and thoughtful UX.',
-          tags: ['UX Resarch, UI, Frontend'],
-          caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-          resourceLink: 'www.google.com',
-          image: '/project-thumbnails/gpay-redesign.jpg'
-        },
-        {
-          id: 4,
-          title: 'Starbound: A Static Site for a Space Tourism Brand',
-          description: 'Designed and developed a concept website with custom layouts and page routing, highlighting creative theming and component reuse.',
-          tags: ['UX Resarch, UI, Frontend'],
-          caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions', 
-          resourceLink: 'www.google.com',
-          image: '/project-thumbnails/gpay-redesign.jpg'
-        }
-    
-      ]
+  const design_projects: Project[] = [
+    {
+      id: 0,
+      title: 'Academic Info System for IIT Roorkee',
+      description: 'Identified communication gaps in IIT Roorkee’s academic ecosystem and designed a centralized platform using user interviews and the Double Diamond approach.',
+      tags: ['UX Research, UI, Frontend'],
+      caseStudyLink: 'https://www.behance.net/gallery/221113241/Academic-Information-System',
+      resourceLink: 'www.google.com',
+      image: '/project-thumbnails/ais-portal.jpg'
+    },
+    {
+      id: 1,
+      title: 'GPay Redesign: Smarter Splits, Clearer Expenses',
+      description: 'An experience-focused redesign that simplifies how users split costs and monitor spending within the Google Pay ecosystem.',
+      tags: ['UX Research, UI, Frontend'],
+      caseStudyLink: 'https://www.behance.net/gallery/211761657/GPay-Redesign',
+      resourceLink: 'www.google.com',
+      image: '/project-thumbnails/gpay-redesign.jpg'
+    },
+    {
+      id: 2,
+      title: 'Researching Mobile Accessibility for the Elderly',
+      description: 'Through user interviews and observational research, we uncovered barriers faced by older adults in mobile usage — culminating in a comprehensive set of accessibility guidelines.',
+      tags: ['UX Research, Frontend'],
+      caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions',
+      resourceLink: 'www.google.com',
+      image: '/project-thumbnails/elderly-ux.jpg'
+    }
+  ]
+
+  // const dev_projects: Project[] = [
+  //   {
+  //     id: 3,
+  //     title: 'A Designer–Developer’s Portfolio, Built with Code',
+  //     description: 'Designed and developed my own portfolio using Next.js, TailwindCSS, and TypeScript — blending aesthetics with performance and thoughtful UX.',
+  //     tags: ['UX Resarch, UI, Frontend'],
+  //     caseStudyLink: 'https://www.behance.net/gallery/213415765/A-UX-case-study-on-senior-friendly-mobile-interactions',
+  //     resourceLink: 'www.google.com',
+  //     image: '/project-thumbnails/portfolio.jpg'
+  //   }
+  // ]
 
   return (
     <div className="snap-start text-background flex flex-col w-full gap-8 py-6 sm:py-16 responsive-padding bg-primary">
-        <h3 className='text-[64px] sm:text-[96px] font-text font-bold'>Projects</h3>
-        <ul className='flex flex-col gap-8 sm:gap-8 items-center'>
-            {project_details
-                .map(project => 
-                    <ProjectCards key={project.id} isReverse={(project.id) % 2 !== 0} project={project}/>
-                )}
-        </ul>
-      </div>
+      <h3 className='text-[64px] sm:text-[96px] font-text font-bold'>Projects</h3>
+      <ul className='flex flex-col gap-8 sm:gap-8 items-center'>
+        {design_projects
+          .map(project =>
+            <ProjectCards key={project.id} isReverse={(project.id) % 2 !== 0} project={project} designCaseStudy={true} />
+          )}
+        {/* {dev_projects
+          .map(project =>
+            <ProjectCards key={project.id} isReverse={(project.id) % 2 !== 0} project={project} designCaseStudy={false} />
+          )} */}
+      </ul>
+    </div>
   )
 }
 
